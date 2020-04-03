d44efad90a8ce6a85e7500faf614b589043ef23c
wxning <wxning@canway.net>
2020-04-03 20:28:36 +0800

    完成engine和parser

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"crawler/fetcher"
-	"fmt"
-	"regexp"
+	"crawler/engine"
+	"crawler/zhenai/parser"
 )
 
 func main() {
-	all, err := fetcher.Fetch("https://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-	printCityList(all)
-}
-
-func printCityList(contents []byte) {
-	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
-	matches := re.FindAllSubmatch(contents, -1)
-	for _, m := range matches {
-		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
-	}
-	fmt.Printf("Matches found: %d\n", len(matches))
+	engine.Run(engine.Request{
+		Url:        "https://www.zhenai.com/zhenghun",
+		ParserFunc: parser.ParseCityList,
+	})
 }
